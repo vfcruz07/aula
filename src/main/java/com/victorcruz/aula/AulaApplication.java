35fc04ab5a5491ab6435dd0cff3f75c3b120d201
Victor Cruz <vfcruz07@gmail.com>
2018-06-06 15:10:31 -0300

    Criando operacao de instanciacao

--- src/main/java/com/victorcruz/aula/AulaApplication.java
+++ src/main/java/com/victorcruz/aula/AulaApplication.java
@@ -1,16 +1,39 @@
 package com.victorcruz.aula;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
+import com.victorcruz.aula.domain.Categoria;
+import com.victorcruz.aula.repositories.CategoriaRepository;
+
 @SpringBootApplication
 @ComponentScan({"com.victorcruz.aula.services", "com.victorcruz.aula.resources"})
 @EntityScan( basePackages = {"com.victorcruz.aula.domain"} )
-public class AulaApplication {
+public class AulaApplication implements CommandLineRunner {
 
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AulaApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria (null, "Informatica");
+		Categoria cat2 = new Categoria (null, "Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+	}
+	
+	
+	
 }
