994ab2e220de1848fbdccc9760d0d34f86e106dd
Victor Cruz <vfcruz07@gmail.com>
2018-06-08 10:25:41 -0300

    Produto e Associacao muitos para muitos

--- src/main/java/com/victorcruz/aula/AulaApplication.java
+++ src/main/java/com/victorcruz/aula/AulaApplication.java
@@ -10,7 +10,9 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
 import com.victorcruz.aula.domain.Categoria;
+import com.victorcruz.aula.domain.Produto;
 import com.victorcruz.aula.repositories.CategoriaRepository;
+import com.victorcruz.aula.repositories.ProdutoRepository;
 
 @SpringBootApplication
 @ComponentScan({"com.victorcruz.aula.services", "com.victorcruz.aula.resources"})
@@ -19,6 +21,9 @@ public class AulaApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(AulaApplication.class, args);
@@ -30,7 +35,22 @@ public class AulaApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria (null, "Informatica");
 		Categoria cat2 = new Categoria (null, "Escritorio");
 		
+		
+		Produto p1 = new Produto(null,"Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto (null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 	}
 	
