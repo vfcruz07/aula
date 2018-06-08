948b89fb487270a7fa8da1c3ca95fdeaa9d9e713
Victor Cruz <vfcruz07@gmail.com>
2018-06-08 16:53:36 -0300

    Commit intermediario aula 18 0:00 a 4:36

---
+++ src/main/java/com/victorcruz/aula/domain/Cidade.java
@@ -0,0 +1,60 @@
+package com.victorcruz.aula.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Cidade implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private String nome;
+	
+	private Estado estado;
+	
+	public Cidade() {
+	}
+
+	public Cidade(Integer id, String nome, Estado estado) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.estado = estado;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Estado getEstado() {
+		return estado;
+	}
+
+	public void setEstado(Estado estado) {
+		this.estado = estado;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	
+	
+}
