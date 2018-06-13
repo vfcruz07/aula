25a9320c37574403254411e408a70a17fbbe8b3d
Victor Cruz <vfcruz07@gmail.com>
2018-06-13 17:04:18 -0300

    Cliente, TipoCliente, telefones e enderecos

--- src/main/java/com/victorcruz/aula/AulaApplication.java
+++ src/main/java/com/victorcruz/aula/AulaApplication.java
@@ -11,10 +11,15 @@ import org.springframework.context.annotation.ComponentScan;
 
 import com.victorcruz.aula.domain.Categoria;
 import com.victorcruz.aula.domain.Cidade;
+import com.victorcruz.aula.domain.Cliente;
+import com.victorcruz.aula.domain.Endereco;
 import com.victorcruz.aula.domain.Estado;
 import com.victorcruz.aula.domain.Produto;
+import com.victorcruz.aula.domain.enums.TipoCliente;
 import com.victorcruz.aula.repositories.CategoriaRepository;
 import com.victorcruz.aula.repositories.CidadeRepository;
+import com.victorcruz.aula.repositories.ClienteRepository;
+import com.victorcruz.aula.repositories.EnderecoRepository;
 import com.victorcruz.aula.repositories.EstadoRepository;
 import com.victorcruz.aula.repositories.ProdutoRepository;
 
@@ -34,6 +39,12 @@ public class AulaApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(AulaApplication.class, args);
@@ -65,6 +76,17 @@ public class AulaApplication implements CommandLineRunner {
 		Cidade c2 = new Cidade(null, "São Paulo", est2);
 		Cidade c3 = new Cidade(null, "Campinas", est2);
 		
+		
+		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		
+		
+		Endereco e1 = new Endereco(null, "Rua Flores", 300, "Apto 303", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", 300, "Sala 800", "Centro", "38777012", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2,c3));
 		
@@ -76,7 +98,9 @@ public class AulaApplication implements CommandLineRunner {
 		
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 		
+		clienteRepository.saveAll(Arrays.asList(cli1));
 		
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 		
 	}
 	
---
+++ src/main/java/com/victorcruz/aula/domain/Cliente.java
@@ -0,0 +1,169 @@
+package com.victorcruz.aula.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.CollectionTable;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.victorcruz.aula.domain.enums.TipoCliente;
+
+@Entity
+public class Cliente implements Serializable {
+
+	/*
+		Implements Serializable - Converte objetos em uma sequencia de bytes
+		
+		serialVersionUID - Necessário para corrigir os warnings do implements Serializable
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	private String nome;
+	private String email;
+	private String cpfOuCnpj;
+	private Integer tipo;
+	
+	
+	/*
+	 * Um cliente possui vários enderecos
+	 * Um endereço pertence a somente 1 cliente
+	 */
+	@OneToMany(mappedBy="cliente")
+	private List<Endereco> enderecos = new ArrayList<>();
+	
+	@ElementCollection
+	@CollectionTable(name="TELEFONE")
+	private Set<String> telefones = new HashSet<>();
+	
+	
+	public Cliente() {
+		/*
+		 * Construtor Vazio
+		 */
+	}
+
+
+	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.cpfOuCnpj = cpfOuCnpj;
+		this.tipo = tipo.getCod();
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getCpfOuCnpj() {
+		return cpfOuCnpj;
+	}
+
+
+	public void setCpfOuCnpj(String cpfOuCnpj) {
+		this.cpfOuCnpj = cpfOuCnpj;
+	}
+
+
+	public TipoCliente getTipo() {
+		return TipoCliente.toEnum(tipo);
+	}
+
+
+	public void setTipo(TipoCliente tipo) {
+		this.tipo = tipo.getCod();
+	}
+
+
+	public List<Endereco> getEnderecos() {
+		return enderecos;
+	}
+
+
+	public void setEnderecos(List<Endereco> enderecos) {
+		this.enderecos = enderecos;
+	}
+
+
+	public Set<String> getTelefones() {
+		return telefones;
+	}
+
+
+	public void setTelefones(Set<String> telefones) {
+		this.telefones = telefones;
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	
+
+	
+	
+	
+	
+}
